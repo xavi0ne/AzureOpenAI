24e63dce177f41a045ec1b18261141ea29b89239
xavi0ne <102357915+xavi0ne@users.noreply.github.com>
2024-05-18 08:07:09 -0400

    Create AzureOpenAIChatCompletion.ps1

---
+++ AzureOpenAIChatCompletion.ps1
@@ -0,0 +1,53 @@
+Connect-AzAccount -environment Azureusgovernment
+
+$keyVaultName = "cmagvashared-kv"
+$apiSecretName = "AOAIKey"
+$secret = Get-AzKeyVaultSecret -VaultName $keyVaultName  -Name $apiSecretName
+$ssPtr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($secret.SecretValue)
+try {
+  $secretValueText = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($ssPtr)
+} finally {
+  [System.Runtime.InteropServices.Marshal]::ZeroFreeBSTR($ssPtr)
+}
+Write-Output $secretValueText
+
+
+$header = [ordered]@{ 
+    "api-key" = $secretValueText
+}
+
+$deploymentName = "dev-gpt"
+$resourceName = "aoai-dev"
+$maxTokens = 800
+$temperature = 0.2
+$top_p = 0.5
+
+do {
+    $userMessage = Read-Host -Prompt "Enter your message"
+
+    $messages = @()
+    $messages += @{
+        role = 'system'
+        content = 'You are a helpful assistant.'
+    }
+    $messages += @{
+        role = 'user'
+        content = $userMessage
+    }
+
+    $body = [ordered]@{
+        messages = $messages
+        max_tokens = $maxTokens
+        temperature = $temperature
+        top_p = $top_p
+    } | ConvertTo-Json
+
+    $uri = "https://$resourceName.openai.azure.us/openai/deployments/$deploymentName/chat/completions?api-version=$apiversion"
+
+    $response = Invoke-RestMethod -Uri $uri -Headers $header -Body $body -Method Post -ContentType 'application/json'
+
+    $response.choices[0].message.content
+     
+    $continue = Read-host -Prompt "Do you want to continue the conversation? (yes/no)"
+} 
+until ($continue -ne "no" -ne "No")
